f17d2aa973ac19687d5c6abb8bfe1fe1220ac48c
light17-droid <khans8962@gmail.com>
2021-06-13 22:25:17 +0530

    responsive header navbar and intro-section

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleNav(){
+    document.getElementById("header-container-id").classList.toggle("header-container-vertical")
+    document.getElementById("header-navbar-id").classList.toggle("header-navbar-vertical");
+}
