ecc94dfa8eb4666d2b79a09aad93c97ccd85e2cb
Md Sujan <khans8962@gmail.com>
2022-06-09 22:43:44 +0530

    light redesign + smtp email

--- script.js
+++ script.js
@@ -1,47 +1,42 @@
-function toggleNav(){
-    document.getElementById("header-container-id").classList.toggle("header-container-vertical")
-    document.getElementById("header-navbar-id").classList.toggle("header-navbar-vertical");
+function toggleNav() {
+  document
+    .getElementById("myHeader")
+    .classList.toggle("header-container-vertical");
+  document
+    .getElementById("header-navbar-id")
+    .classList.toggle("header-navbar-vertical");
 }
 
-const myForm = document.getElementById('myForm');
+const myForm = document.forms["myForm"];
 
-myForm.addEventListener('submit', validateForm);
+myForm.addEventListener("submit", validateForm);
 
-function validateForm(e){
-    e.preventDefault();
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.email.value))
-    { 
-        const formData = {
-            userName: myForm.children[0].value,
-            userEmail: myForm.children[1].value,
-            userMessage: myForm.children[2].value
-        }
-        console.log(formData);
-        fetch(
-            "https://portfoliodb-ba460-default-rtdb.firebaseio.com/contactFormDB.json",
-            {
-                method: 'POST',
-                body: JSON.stringify(formData),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }
-        );
-        myForm.reset();
-        const message = document.createElement('div');
-        message.innerText = "Done";
-        message.style.color = "rgb(0,255,0)";
-        myForm.insertBefore(message, myForm.childNodes[0]); 
-        return (true)
-    }
-    else
-    {
-        console.log("You have entered an invalid email address!");
-        const message = document.createElement('div');
-        message.innerText = "Error in email id input";
-        message.style.color = "rgb(255,0,0)";
-        myForm.insertBefore(message, myForm.childNodes[0]);     // Append <li> to <ul> with id="myList"
-        myForm.reset();
-        return (false)
-    }
+function validateForm(e) {
+  e.preventDefault();
+  if (
+    /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm["email"].value)
+  ) {
+    const formData = {
+      userName: myForm["name-input"].value,
+      userEmail: myForm["email-input"].value,
+      userMessage: myForm["message"].value,
+    };
+    Email.send({
+      Host: "smtp.elasticemail.com",
+      Username: "mysmtp@elasticemail.com",
+      Password: "4B72FCC06A5E507DC28494C71E1DD9FF7C82",
+      To: "khans8962@gmail.com",
+      From: "khans8962@gmail.com",
+      Subject: "Contact Form Message from" + formData.userEmail,
+      Body: "Message:\n" + formData.userMessage,
+    }).then((message) => alert(message));
+    console.log(formData);
+    myForm.reset();
+
+    return true;
+  } else {
+    console.log("You have entered an invalid email address!");
+    myForm.reset();
+    return false;
+  }
 }
